de647edaffb2664af28f68bb48f7daf71ea89f82
LouisMazel <me@loicmazuel.com>
2024-03-14 17:36:41 +0100

    chore: setup project - add linters, editor config and typescript config

---
+++ .eslintrc.cjs
@@ -0,0 +1,57 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:promise/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    'no-console': [
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    ],
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
+    'no-duplicate-imports': 'error',
+    // TYPESCRIPT
+    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    // UNICORN
+    'unicorn/prefer-module': 'off',
+    // PRETTIER
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+}
